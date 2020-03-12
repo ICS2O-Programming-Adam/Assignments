4365cfb932f6b3ca155450f878f764f993f9f8d5
s445348 <adam.winogron@ocsbstudent.ca>
2020-03-11 22:10:51 -0400

    worked on assignment 4

--- AnimatingImagesAdam/main.lua
+++ AnimatingImagesAdam/main.lua
@@ -48,7 +48,9 @@ Runtime:addEventListener("enterFrame", MoveMike)
 
 -- display a text box explaining the first part of the story
 local text1 = display.newText( "One day, Mike was walking home and stumbled across \n".. 
-	" an abandonned mayan temple...", 500, 200, nil, 35)
+	" an abandonned mayan temple, where 2 giants appeared, \n"
+	.. "so he had to fight them off! Luckily, \n" 
+	.. "he knows how to shoot lasers with his eyes!", 500, 100, nil, 35)
 
 -- set the colour of the text
 text1:setTextColor( 247/255, 60/255, 22/255 )
@@ -57,14 +59,14 @@ text1:setTextColor( 247/255, 60/255, 22/255 )
 local robloxCharlotte = display.newImageRect("Images/robloxcharlotte.png", 300, 300 )
 
 -- set the location of the character
-robloxCharlotte.x = 200
+robloxCharlotte.x = 150
 robloxCharlotte.y = 0
 
 -- make the image transparent
 robloxCharlotte.alpha = 0
 
 -- make a second scroll speed
-scrollSpeed2 = 5
+scrollSpeed2 = 10
 
 -- function: MoveCharlotte
 -- input: this function accepts an event listener
@@ -74,7 +76,7 @@ local function MoveCharlotte(event)
 	-- add the scroll speed to the x value of robloxPrisonMike
 	robloxCharlotte.y = robloxCharlotte.y + scrollSpeed2
 	-- change the transparency of robloxPrisonMike every time it moves so that it fades input
-	robloxCharlotte.alpha = robloxCharlotte.alpha + 0.01
+	robloxCharlotte.alpha = robloxCharlotte.alpha + 0.02
 	if (robloxCharlotte.y >= 475) then
 		scrollSpeed2 = 0
 	end
@@ -82,3 +84,68 @@ end
 
 -- MoveCharlotte will be called over and over again
 Runtime:addEventListener("enterFrame", MoveCharlotte)
+
+-- flip the character horizontally
+robloxCharlotte:scale(-1,1)
+
+-- create the third character
+local robloxPeter = display.newImageRect("Images/robloxpetergriffin.png", 300, 300 )
+
+-- set the location of the character
+robloxPeter.x = 400
+robloxPeter.y = 800
+
+--make the image transparent
+robloxPeter.alpha = 0
+
+-- flip the character horizontally
+robloxPeter:scale(-1,1)
+
+-- set the scroll speed
+scrollSpeed3 = -10
+
+-- function: MovePeter
+-- input: this function accepts an event listener
+-- output: none
+-- description: this function adds the scroll speed to the y-value of the character
+local function MovePeter(event)
+	-- add the scroll speed to the x value of robloxPrisonMike
+	robloxPeter.y = robloxPeter.y + scrollSpeed3
+	-- change the transparency of robloxPrisonMike every time it moves so that it fades input
+	robloxPeter.alpha = robloxPeter.alpha + 0.02
+	if (robloxPeter.y <= 475) then
+		scrollSpeed3 = 0
+	end
+end
+
+-- MovePeter will be called over and over again
+Runtime:addEventListener("enterFrame", MovePeter)
+
+-- create the laser
+local laser = display.newRect( 610, 400, 350, 10)
+
+-- change the colour of the laser
+laser:setFillColor(1, 0, 0)
+
+-- make the laser invisible
+laser.alpha = 0
+
+-- function: ShootLaser
+-- input: this function accepts an event listener
+-- output: none
+-- description: this function makes the laser appear
+local function ShootLaser(event)
+	if (robloxPrisonMike.x <= 800) then
+		laser.alpha = 1
+	end
+end
+
+Runtime:addEventListener("enterFrame", ShootLaser)
+
+local function StopLaser(event)
+	if (laser.alpha == 1) then
+		laser.alpha = laser.alpa
+	end
+end
+
+Runtime:addEventListener("enterFrame", StopLaser)
