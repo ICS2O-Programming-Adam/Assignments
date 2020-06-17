8cdaf51cee178f03949251f0e2eb960812b6f403
s445348 <adam.winogron@ocsbstudent.ca>
2020-06-17 04:45:11 -0400

    A

--- Level1Adam/final_screen.lua
+++ Level1Adam/final_screen.lua
@@ -27,19 +27,50 @@ local scene = composer.newScene( sceneName )
 -----------------------------------------------------------------------------------------
  
 -- The local variables for this scene
+local astro
 local kingKoko
+local kokoArms
+local blaster
 local kokoSpeed = 20
+local kokoSpeed2 = 10
+local glueSpeed = 15
+local lastSpeed = 5
+local shrinkSpeed = 10
+local superShrinkSpeed = 20
+local alphaSpeed = 0.001
+local heightSpeed = 5
+local widthSpeed = 10
+local starSpeedX = -15
+local starSpeedY = 15
 local speech1
 local speech2
 local speech3
 local platShrink = 20
 local kokoShrink = 15
 local speechShrink = 10
-
+local astroSpeed = 10
 
 --------------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------
+
+local function ReStar(event)
+    if (shootingStar.x <= 0) then
+        shootingStar.x = math.random ( 1, 1024 )
+        shootingStar.y = 0
+    end
+end
+
+local function MoveStar(event)
+    shootingStar.x = shootingStar.x + starSpeedX
+    shootingStar.y = shootingStar.y + starSpeedY
+    shootingStar.isVisible = true
+end
+
+local function CallStar()
+    Runtime:addEventListener( "enterFrame", MoveStar)    
+    Runtime:addEventListener( "enterFrame", ReStar)
+end
 
 local function MoveKoko(event)
     kingKoko.y = kingKoko.y + kokoSpeed
@@ -50,20 +81,121 @@ local function MoveKoko(event)
 end
 
 local function TextDisplay()
+    speech1.x = 1400
     speech1.isVisible = false
     speech2.isVisible = true
+    kingKoko.isVisible = false
+    kokoArms.isVisible = true
+    moonGlue.isVisible = true
 end
 
 local function TextDisplay2()
     speech2.isVisible = false
     speech3.isVisible = true
+    kokoArms:scale(-1,1)
+    kokoArms.x = kokoArms.x + 200
+    moonGlue.width = moonGlue.width / 2
+    moonGlue.height = moonGlue.height / 2
+    moonGlue.x = moonGlue.x + 200
 end
 
-local function TextDisplay3()
+local function MoveAstro(event)
+    astro.x = astro.x + lastSpeed
+    if (astro.x >= 500) then
+        lastSpeed = 0
+        moonGlue.x = 390
+        moonGlue.y = 270
+    end
+end
+
+local function TurnGlue()
+    moonGlue:rotate(90)
+end
+
+local function Shrink(event)
+    astro.width = astro.width - shrinkSpeed
+    astro.height = astro.height - shrinkSpeed
+    moonGlue.width = moonGlue.width - shrinkSpeed
+    moonGlue.height = moonGlue.height - shrinkSpeed
+    platform.width = platform.width - superShrinkSpeed
+    platform.height = platform.height - shrinkSpeed
+end
+
+local function CallShrink()
+    Runtime:addEventListener("enterFrame", Shrink)
+end
+
+local function Hooray(event)
+    winText.alpha = winText.alpha + alphaSpeed
+    winText.width = winText.width + widthSpeed
+    winText.height = winText.height + heightSpeed
+    if (winText.width >= 990) and (winText.height >= 495) then
+        alphaSpeed = 0
+        widthSpeed = 0
+        heightSpeed = 0
+    end
+end
+
+
+
+local function CallHooray()
+    Runtime:addEventListener("enterFrame", Hooray)
+end
+
+local  function CallAstro()
+    Runtime:addEventListener("enterFrame", MoveAstro)
+    timer.performWithDelay(600, TurnGlue)
+    timer.performWithDelay(2000, CallShrink)
+    timer.performWithDelay(2500, CallHooray)
+    timer.performWithDelay(3000, CallStar)
+end
+
+local function ByeKoko(event)
     speech3.isVisible = false
-    speech4.isVisible = true
+    kokoArms.x = kokoArms.x + kokoSpeed2
+    kokoArms.y = kokoArms.y - kokoSpeed2
+    kokoArms:rotate(10)
+    blaster.isVisible = false
+    moonGlue.y = moonGlue.y + glueSpeed
+    if (moonGlue.y >= 400) then
+        glueSpeed = 0
+    end
 end
 
+local function QueueKoko()
+    Runtime:addEventListener("enterFrame", ByeKoko)
+end
+
+local function OffLazer()
+    laser.isVisible = false
+    timer.performWithDelay(10, QueueKoko)
+end
+
+local function ShootLazer(event)
+    Runtime:removeEventListener(StopMSM)
+    laser.isVisible = true
+    timer.performWithDelay(500, OffLazer)
+end
+
+local function StopMSM()
+    Runtime:removeEventListener(CallMSM)
+    Runtime:removeEventListener(MoveKoko)
+    Runtime:removeEventListener(MoveSpaceMan)
+end
+
+local  function MoveSpaceMan()
+    astro.isVisible = true
+    astro.x = astro.x + astroSpeed
+    if (astro.x >= 300) then
+        astroSpeed = 0
+        blaster.isVisible = true
+        StopMSM()
+    end
+end
+
+local function CallMSM()
+    Runtime:addEventListener("enterFrame", MoveSpaceMan)
+end
 
 
 -- The function that will go to the main menu 
@@ -91,15 +223,60 @@ function scene:create( event )
     -- insert into scene group
     sceneGroup:insert( bkg )
 
-    plat2 = display.newImageRect("Images/platform2.png", 1000, 299)
-    plat2.x = 500
-    plat2.y = 500
-    sceneGroup:insert(plat2)
+    platform = display.newImageRect("Images/platform2.png", 1000, 299)
+    platform.x = 500
+    platform.y = 500
+    sceneGroup:insert(platform)
 
     kingKoko = display.newImageRect("Images/spacechimp.png", 360, 450)
     kingKoko.x = 500
     kingKoko.y = -200
     sceneGroup:insert(kingKoko)
+
+    kokoArms = display.newImageRect("Images/spacechimp2.png", 526.5, 487.5)
+    kokoArms.x = 500
+    kokoArms.y = 250
+    kokoArms.isVisible = false
+    sceneGroup:insert(kokoArms)
+
+    astro = display.newImageRect("Images/astronaut.png", 311.5, 360.5)
+    astro.x = -100
+    astro.y = 250
+    astro:scale(-1,1)
+    astro.isVisible = false
+    sceneGroup:insert( astro )
+
+    blaster = display.newImageRect("Images/lasergun.png", 214.5, 88.5)
+    blaster.x = 150
+    blaster.y = 250
+    blaster.isVisible = false
+    sceneGroup:insert( blaster )
+
+    moonGlue = display.newImageRect("Images/moonglue.png", 174, 174)
+    moonGlue.x = 320
+    moonGlue.y = 210
+    moonGlue:rotate(-45)
+    moonGlue.isVisible = false
+    sceneGroup:insert(moonGlue)
+
+    shootingStar = display.newImageRect("Images/shootstar.png", 210, 148.5)
+    shootingStar.x = 700
+    shootingStar.y = 0
+    shootingStar.isVisible = false
+    sceneGroup:insert( shootingStar )
+
+    winText = display.newImageRect("Images/CompleteText.png", 10, 5)
+    winText.x = 512
+    winText.y = 300
+    winText.aplha = 0
+    sceneGroup:insert(winText)
+
+    laser = display.newImageRect("Images/lazer.png", 163, 528)
+    laser.x = 480
+    laser.y = 230
+    laser:rotate(90)
+    laser.isVisible = false
+    sceneGroup:insert( laser )
 
     speech1 = display.newImageRect("Images/NewText1.png", 400, 200)
     speech1.y = 100
@@ -113,9 +290,9 @@ function scene:create( event )
     speech2.isVisible = false
     sceneGroup:insert(speech2)
 
-    speech3 = display.newImageRect("Images/text3.png", 400, 200)
+    speech3 = display.newImageRect("Images/NewText3.png", 400, 200)
     speech3.y = 100
-    speech3.x = 800
+    speech3.x = 870
     speech3.isVisible = false
     sceneGroup:insert(speech3)
 
@@ -124,7 +301,6 @@ function scene:create( event )
     speech4.x = 800
     speech4.isVisible = false
     sceneGroup:insert(speech4)
-
 
 end -- function scene:create( event )
 
@@ -157,12 +333,11 @@ function scene:show( event )
 
         timer.performWithDelay ( 5000, TextDisplay2 )
 
-        timer.performWithDelay ( 7000, TextDisplay3 )
+        timer.performWithDelay ( 7000, CallMSM ) 
 
-        timer.performWithDelay ( 8000, CallShrink )
+        timer.performWithDelay ( 8000, ShootLazer ) 
 
-        -- Go to the main menu screen after the given time.
-        timer.performWithDelay ( 9000, gotoLevel1)     
+        timer.performWithDelay ( 9000, CallAstro )  
     end
 
 end --function scene:show( event )
@@ -190,6 +365,8 @@ function scene:hide( event )
 
         Runtime:removeEventListener(MoveKoko)
         Runtime:removeEventListener(Shrink)
+        Runtime:removeEventListener(MoveStar)
+        Runtime:removeEventListener(ReStar)
     end
 
 end --function scene:hide( event )
--- Level1Adam/main.lua
+++ Level1Adam/main.lua
@@ -18,4 +18,4 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "final_screen" )
+composer.gotoScene( "splash_screen" )
